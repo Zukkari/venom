84158faf1ec01a914b65d34d96303dcb2c063467
Zukkari <stanislav.myshkovski@gmail.com>
2019-01-02 18:06:38 +0200

    Initial commit

---
+++ src/main/kotlin/com/github/zukkari/Hello.kt
@@ -0,0 +1,6 @@
+package com.github.zukkari
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/github/zukkari/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.github.zukkari
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
