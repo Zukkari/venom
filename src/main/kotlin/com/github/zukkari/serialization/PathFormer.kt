a0c7ce25ee3986ea070b172615ae9eb301980c4a
Zukkari <stanislav.myshkovski@gmail.com>
2019-01-07 19:59:40 +0200

    Add path generator

--- src/main/kotlin/com/github/zukkari/serialization/PathFormer.kt
+++ src/main/kotlin/com/github/zukkari/serialization/PathFormer.kt
@@ -5,4 +5,23 @@ import java.io.File
 internal object PathFormer {
 
     fun form(agentClass: Class<*>): String = "${agentClass.canonicalName.replace(".", File.separator)}.class"
+
+    fun getSubDirectories(agentClass: Class<*>): List<String> {
+        tailrec fun getPaths(split: List<String>, acc: List<String>, result: List<String>): List<String> {
+            return if (split.isEmpty()) {
+                result
+            } else {
+                val curr = split[0]
+                val newPath = if (acc.isEmpty()) curr else acc.joinToString(separator = File.separator) + File.separator + curr
+                val addedEnd = if (split.size > 1) newPath + File.separator else newPath
+
+                getPaths(split.subList(1, split.size), acc + curr, result + addedEnd)
+            }
+        }
+
+        val formed = form(agentClass)
+        val split = formed.split(File.separator)
+
+        return getPaths(split, listOf(), listOf())
+    }
 }
--- src/test/kotlin/com/github/zukkari/serialization/PathFormerTest.kt
+++ src/test/kotlin/com/github/zukkari/serialization/PathFormerTest.kt
@@ -3,6 +3,7 @@ package com.github.zukkari.serialization
 import com.github.zukkari.loaders.implementation.FileSystemLoader
 import org.junit.Test
 import org.junit.jupiter.api.TestInstance
+import java.io.File
 import kotlin.test.assertEquals
 
 
@@ -14,9 +15,24 @@ class PathFormerTest {
         val path = PathFormer.form(FileSystemLoader::class.java)
 
         assertEquals(
-            "com/github/zukkari/loaders/implementation/FileSystemLoader.class",
+            "com${File.separator}github${File.separator}zukkari${File.separator}loaders${File.separator}implementation${File.separator}FileSystemLoader.class",
             path,
             "Path should be correctly formed!"
+        )
+    }
+
+    @Test
+    fun `Subdirectories get correctly formed`() {
+        val path = PathFormer.getSubDirectories(PathFormerTest::class.java)
+
+        assertEquals(
+            listOf(
+                "com${File.separator}",
+                "com${File.separator}github${File.separator}",
+                "com${File.separator}github${File.separator}zukkari${File.separator}",
+                "com${File.separator}github${File.separator}zukkari${File.separator}serialization${File.separator}",
+                "com${File.separator}github${File.separator}zukkari${File.separator}serialization${File.separator}PathFormerTest.class"
+                ), path
         )
     }
 
